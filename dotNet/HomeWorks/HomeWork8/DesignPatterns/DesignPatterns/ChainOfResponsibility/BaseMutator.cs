0f24b24f766479778b11250cc21bba2274e22eae
Vadym Mulish <kabanborov43346@gmail.com>
2021-02-11 23:48:20 +0200

    Added implementations for DesignPatterns

---
+++ dotNet/HomeWorks/HomeWork8/DesignPatterns/DesignPatterns/ChainOfResponsibility/BaseMutator.cs
@@ -0,0 +1,7 @@
+﻿namespace DesignPatterns.ChainOfResponsibility
+{
+    public class BaseMutator
+    {
+        
+    }
+}
---
+++ dotNet/HomeWorks/HomeWork8/DesignPatterns/DesignPatterns/IoC/ServiceDescriptor.cs
@@ -0,0 +1,16 @@
+﻿namespace DesignPatterns.IoC
+{
+    public class ServiceRegistrationDescriptor<T> : IServiceRegistrationDescriptor
+    {
+        private T _type;
+        private Lifetime _lifetime;
+
+        public ServiceRegistrationDescriptor(T type, Lifetime lifetime)
+        {
+            _type = type;
+            _lifetime = lifetime;
+        }
+        
+        
+    }
+}
